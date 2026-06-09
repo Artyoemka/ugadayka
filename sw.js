4251c4c9f9aeb72ba657099924e277983e03c1c4
Artyoemka <artem.kolosov151103@gmail.com>
2026-06-09 21:31:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ugadayka-dc30af67';
+const CACHE = 'ugadayka-7edee312';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
